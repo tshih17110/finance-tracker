bc758aa867fdef2e2361c16f9b8a5226b67d4a47
Thomas Shih <tshih17110@gmail.com>
2023-09-29 23:52:21 +0800

    Refactor balance component to use React hooks

--- client/src/components/Balance.jsx
+++ client/src/components/Balance.jsx
@@ -1,53 +1,100 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-class Balance extends Component {
-	constructor() {
-		super();
-		this.state = {
-			balances: [],
-			isLoading: true,
-		}
-	}
+function Balance() {
 
-	componentDidMount = async() => {
-		try {
-			const response = await axios.post("/api/balance", {
-				access_token: sessionStorage.getItem("accessToken"),
-			});
-			this.setState({
-				balances: response.data.Balance,
-				isLoading: false,
-			});
-		} catch (error) {
-			console.error("Error fetching balances: ", error);
-			this.setState({
-				isLoading: false, 
-			});            
-		}
-	}
+	const [balances, setBalances] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-	render() {
-		const { balances, isLoading } = this.state;
-		return (
-			<div>
-				<h2>Account Balances</h2>
-				{isLoading ? ( 
-					<p>Loading...</p>
-				) : balances.accounts && balances.accounts.length > 0 ? (
-					<ul>
-						{balances.accounts.map((account, index) => (
-							<li key={index}>
-								{account.name}: ${parseFloat(account.balances.current).toFixed(2)}
-							</li>
-						))}
-					</ul>
-				) : (
-					<p>No accounts available.</p>
-				)}
-			</div> 
-		)
-	}
+	useEffect(() => {
+		const fetchBalances = async () => {
+			try {
+				const response = await axios.post("/api/balance", {
+					access_token: sessionStorage.getItem("accessToken"),
+				});
+				setBalances(response.data.Balance);
+				setIsLoading(false);
+			} catch (error) {
+				console.error("Error fetching account balances: ", error);
+				setIsLoading(false);
+			}
+		}
+		fetchBalances();
+	}, []);
+
+	return (
+		<div>
+			<h2>Account Balances</h2>
+			{isLoading ? ( 
+				<p>Loading...</p>
+			) : balances.accounts && balances.accounts.length > 0 ? (
+				<ul>
+					{balances.accounts.map((account, index) => (
+						<li key={index}>
+							{account.name}: ${parseFloat(account.balances.current).toFixed(2)}
+						</li>
+					))}
+				</ul>
+			) : (
+				<p>No accounts available.</p>
+			)}
+		</div> 
+	)
+	
 }
 
 export default Balance;
+
+// import React, { Component } from 'react';
+// import axios from 'axios';
+
+// class Balance extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			balances: [],
+// 			isLoading: true,
+// 		}
+// 	}
+
+// 	componentDidMount = async() => {
+// 		try {
+// 			const response = await axios.post("/api/balance", {
+// 				access_token: sessionStorage.getItem("accessToken"),
+// 			});
+// 			this.setState({
+// 				balances: response.data.Balance,
+// 				isLoading: false,
+// 			});
+// 		} catch (error) {
+// 			console.error("Error fetching balances: ", error);
+// 			this.setState({
+// 				isLoading: false, 
+// 			});            
+// 		}
+// 	}
+
+// 	render() {
+// 		const { balances, isLoading } = this.state;
+// 		return (
+// 			<div>
+// 				<h2>Account Balances</h2>
+// 				{isLoading ? ( 
+// 					<p>Loading...</p>
+// 				) : balances.accounts && balances.accounts.length > 0 ? (
+// 					<ul>
+// 						{balances.accounts.map((account, index) => (
+// 							<li key={index}>
+// 								{account.name}: ${parseFloat(account.balances.current).toFixed(2)}
+// 							</li>
+// 						))}
+// 					</ul>
+// 				) : (
+// 					<p>No accounts available.</p>
+// 				)}
+// 			</div> 
+// 		)
+// 	}
+// }
+
+// export default Balance;
