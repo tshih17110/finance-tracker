f0f460ae916636f1c43d637db1af8410819e9947
Thomas Shih <tshih17110@gmail.com>
2025-04-09 00:13:19 +0800

    Add deployment support

--- server/server.js
+++ server/server.js
@@ -7,6 +7,15 @@ app.use(express.json());
 const plaidRouter = require('./plaidRouter');
 app.use('/api', plaidRouter.router);
 
+//Production server
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(_dirname, '../client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(_dirname, '../client/build', 'index.html'));
+    });
+}
+
+
 app.listen(PORT, () => {
 	console.log(`Server running on ${PORT}`);
 });
