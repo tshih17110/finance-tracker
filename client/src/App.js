848b548bad4b20e67d696e1832a386fe852b6187
Thomas Shih <tshih17110@gmail.com>
2025-04-09 01:10:35 +0800

    Fix issues with deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,19 @@
 import './App.css';
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Link from './components/Link';
 import Dashboard from './components/Dashboard';
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Routes>
-					<Route exact path="/link" element={<LinkPage/>} />
-					<Route path="/dashboard" element={<DashboardPage/>} />
-				</Routes>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Link />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-
-const LinkPage = () => {
-	return (
-		<div>
-			<Link />
-		</div>
-	);
-};
-
-const DashboardPage = () => {
-	return (
-		<div>
-            <Dashboard />
-		</div>
-	);
-};
-
 
 export default App;
